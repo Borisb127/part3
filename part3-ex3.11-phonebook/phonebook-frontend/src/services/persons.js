2e3f7dbbf9a7f988350b720b06f0bc9ec2b2b141
Borisb127 <boris.borochov92@gmail.com>
2024-09-01 02:35:29 +0300

    Updated baseUrl to Render URL

--- part3-ex3.11-phonebook/phonebook-frontend/src/services/persons.js
+++ part3-ex3.11-phonebook/phonebook-frontend/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = '/api/persons';
+const baseUrl = 'https://phonebook-ex3-11-bkwc.onrender.com/api/persons';
 
 const getAll = () => {
     const request = axios.get(baseUrl)
